17b9a3dccbdfe5d086026a7b5d443527bd28b007
Ronan Killeen <rkilleen@gmail.com>
2018-10-30 18:24:24 +0000

    Use the json package to set the response

--- estimator/rest_controller.py
+++ estimator/rest_controller.py
@@ -1,21 +1,21 @@
-from flask import Response
+from flask import json, Response
 from estimator import app
 
 @app.route('/rest/v1/group/<groupname>', methods = ['POST'])
 def create_group(groupname):
 	# TODO: implement
 	if groupname == 'NewGroup':
-		body = '{ "message" : "Group created" }'
+		body = { "message" : "Group created" }
 		code = 201
 	else:
-		body = '{ "message" : "Group exists" }'
+		body = { "message" : "Group already exists" }
 		code = 400
-	return Response(body, status=code, mimetype='application/json')
+	return Response(json.dumps(body), status=code, mimetype='application/json')
 
 @app.route('/rest/v1/group/<groupname>', methods = ['GET'])
 def query_group(groupname):
 	# TODO: implement
 	if groupname == 'TestGroup':
-		return '{ "groupname" : "' + groupname + '" }'
+		return json.dumps({ "groupname" : groupname })
 	else:
-		return Response('{ "message" : "group not found"', 404, mimetype='application/json')
+		return Response(json.dumps({ "message" : "Group not found"}), 404, mimetype='application/json')
