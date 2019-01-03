b1f17c5128746a6f00f504451f80b2b33fcd4e70
Ronan Killeen <rkilleen@gmail.com>
2019-01-03 12:47:55 +0000

    Adding flask-migrate and getting it ready to initialise the migration

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-from estimator import create_app
+from estimator import create_app, db
+from flask_migrate import Migrate
 import os
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run()
