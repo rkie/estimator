1761fc4edcedd4d4f06f5cf506a1887b3475edc6
Ronan Killeen <rkilleen@gmail.com>
2019-01-14 16:45:28 +0000

    adding the model changes and the alembic migration script

--- database/models.py
+++ database/models.py
@@ -2,12 +2,26 @@ from estimator import db
 
 class Group(db.Model):
 	"""A group that team members can join and contains issues"""
-
+	__tablename__ = 'estimation_group'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(32))
+	user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 	def __init__(self, name):
 		self.name = name
 
 	def __repr__(self):
 		return '<Estimation group: {}>'.format(self.name)
+
+
+class User(db.Model):
+	"""A user that can create groups and issues for estimation"""
+
+	id = db.Column(db.Integer, primary_key=True)
+	nickname = db.Column(db.String(32), nullable=False)
+
+	def __init__(self, nickname):
+		self.nickname = nickname
+
+	def __repr__(self):
+		return self.nickname
