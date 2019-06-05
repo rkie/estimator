b5a456b3d38b2998016b2172254a72a5b015e6dc
Ronan Killeen <rkilleen@gmail.com>
2019-06-05 12:59:26 +0100

    Adding Flask-Login and wiring up with app

--- estimator/__init__.py
+++ estimator/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'web.login'
 
 def create_app(config_name):
 	app = Flask(__name__)
@@ -14,6 +17,7 @@ def create_app(config_name):
 	config[config_name].init_app(app)
 	bootstrap.init_app(app)
 	db.init_app(app)
+	login_manager.init_app(app)
 
 	from .controllers import web as web_blueprint
 	app.register_blueprint(web_blueprint)
